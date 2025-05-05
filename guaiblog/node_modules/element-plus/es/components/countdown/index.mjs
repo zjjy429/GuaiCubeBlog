3184ffad3da1ab133f47e7664867e75cba6ec1cf
3253446781@qq.com <3253446781@qq.com>
2025-05-05 22:57:22 +0800

    'delete'

--- guaiblog/node_modules/element-plus/es/components/countdown/index.mjs
+++
@@ -1,8 +0,0 @@
-import Countdown from './src/countdown.mjs';
-export { countdownEmits, countdownProps } from './src/countdown2.mjs';
-import { withInstall } from '../../utils/vue/install.mjs';
-
-const ElCountdown = withInstall(Countdown);
-
-export { ElCountdown, ElCountdown as default };
-//# sourceMappingURL=index.mjs.map
--- guaiblog/node_modules/element-plus/es/components/radio/src/radio-button2.mjs
+++
@@ -1,9 +0,0 @@
-import { radioPropsBase } from './radio.mjs';
-import { buildProps } from '../../../utils/vue/props/runtime.mjs';
-
-const radioButtonProps = buildProps({
-  ...radioPropsBase
-});
-
-export { radioButtonProps };
-//# sourceMappingURL=radio-button2.mjs.map
